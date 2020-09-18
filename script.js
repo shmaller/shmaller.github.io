24f6d1e00f597d70a69c1140c94eff983f8bc815
shmaller <nicholasboni94@gmail.com>
2020-09-18 17:10:56 -0400

    Overhaul

---
+++ script.js
@@ -0,0 +1,17 @@
+function menu_icon_animation(x){
+	x.classList.toggle("change");
+}
+
+function operate_menu(){
+	if(document.getElementById("menu").style.width == "0px"){
+		document.getElementById("menu").style.width = "250px";
+		document.getElementById("main").style.marginLeft = "250px";
+	} else{
+		document.getElementById("menu").style.width = "0px";
+		document.getElementById("main").style.marginLeft = "0px";
+	}
+}
+
+function show_page_content(){
+	document.getElementById("hidden_content").style.display="block";
+}
